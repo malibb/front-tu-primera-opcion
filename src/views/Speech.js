0478617983a7658c22120d5199413a425b8fa028
Malinali <mali@devf.mx>
2019-10-27 13:02:35 -0600

    cosas

--- src/views/Speech.js
+++ src/views/Speech.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     < Layout head = "Tu primera opción!"
     subhead = "Porque necesitas a alguien que valga la pena." >
-         <MyFrame src="https://malibb.github.io/textSpeech/" height="700" width="1500"/>
+         <MyFrame src="https://malibb.github.io/textSpeech/" height="700" width="1300"/>
     </Layout>
   );
 }
